3829671af971b94e55de2986932b336c75d805a3
Eric <ericquintero2002@gmail.com>
2020-11-18 12:01:06 +0100

    HOLA

---
+++ P413_ClarasYOscuras.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author ericq
+ * Programa para saber el número de losetas
+ * 18/11/2020
+ * Joan Peñarrocha Crespo
+ * 1º DAM
+ */
+
+public class P413_ClarasYOscuras {
+
+	/**
+	 * @param args
+	 * Saber el número de losetas
+	 */
+	
+	public static void main(String[] args) {
+		
+		//Añadimos el escaner
+		Scanner teclado = new Scanner(System.in);
+		
+		//Declaramos las variables
+		int oscuras, claras;
+		
+		//Cerramos el escaner
+		teclado.close();
+
+	}
+
+}
